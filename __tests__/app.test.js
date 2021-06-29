81ddb1bac0fd9be86d4af282abac7f4ee29bd241
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-06-29 16:39:13 -0700

    set up folders and first post test failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,27 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
+const agent = request.agent(app);
+
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('signs up a user via POST', async() => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      email: 'test@test.com'
+    });
+  });
+
+
+
 });
